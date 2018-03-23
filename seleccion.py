e35df63bbbf759388e52612846ff068c2a760532
Criofilaxx <crisofilaxx@gmail.com>
2018-03-23 00:17:08 -0400

    Fix fallo en indices

--- seleccion.py
+++ seleccion.py
@@ -9,8 +9,8 @@ def minimo(vector, a_partir_de=0):
     for indice, valor in enumerate(vector[a_partir_de:]):
         if valor < vector[indice_min]:
             logging.info('indice_min era {}, y ahora es {}'.format(indice_min, indice))
-            indice_min = indice 
-    return indice_min + a_partir_de # corro el indice en caso de que sea necesario
+            indice_min = indice + a_partir_de # corro el indice en caso de que sea necesario
+    return indice_min
 
 def intercambia(vector, indice_a, indice_b):
     vector[indice_a], vector[indice_b] = vector[indice_b], vector[indice_a]
