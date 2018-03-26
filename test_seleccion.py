df93c2ce596b42e0b4ea86af1275f5f7f24e3bd6
Criofilaxx <crisofilaxx@gmail.com>
2018-03-26 02:04:24 -0400

    prueba de que merge funciona con vectores vacios

--- test_seleccion.py
+++ test_seleccion.py
@@ -27,4 +27,9 @@ class Seleccion(unittest.TestCase):
         arreglo2 = [1,1,7,8]
         final =   [1,1,2,2,5,6,7,8]
         self.assertEqual(merge(arreglo,arreglo2), final)
-        
+
+    def test_merge_funciona_arreglo_vacio(self):
+        arreglo  = []
+        arreglo2 = [1,1,7,8]
+        final = [1,1,7,8]
+        self.assertEqual(merge(arreglo,arreglo2), final)
