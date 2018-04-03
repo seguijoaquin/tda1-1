192c3ac2b14a217bd98a255a9c346ca4653b112e
Criofilaxx <crisofilaxx@gmail.com>
2018-04-03 01:47:39 -0400

    generador de arreglos

--- graficador.py
+++ graficador.py
@@ -1,6 +1,11 @@
 # coding: utf-8
 import time
+import random
+
 def performance(vector, algoritmo):
     inicio = time.time()
     algoritmo(vector)
     return time.time()-inicio
+
+def gen_vector(length):
+    return [random.randrange(1000) for _ in range(length)]
